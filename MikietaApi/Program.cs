71678b46f342268365a4ff04f0d19b761c34d5d9
Patryk Wilk <patryk.wilk@codeconcept.pl>
2024-04-05 00:32:02 +0200

    MIK-54 changed route for fetching products

--- MikietaApi/Program.cs
+++ MikietaApi/Program.cs
@@ -1,5 +1,3 @@
-using Microsoft.AspNetCore.Http.HttpResults;
-
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors(options =>
@@ -15,76 +13,26 @@ app.UseCors("MyPolicy");
 app.MapGet("/", () => new[] { "a", "b", "c" });
 
 
-app.MapGet("/pizza", (string size) =>
-{
-    if (size == "small")
-    {
-        return Results.Ok(new[] { Margherita(17), Cipolla(18), Funghi(18), Salami(18), FunghiEProsciutto(18) });
-    }
-
-    if (size == "medium")
-    {
-        return Results.Ok(new[] { Margherita(22), Cipolla(23), Funghi(23), Salami(26), FunghiEProsciutto(26) });
-    }
-
-    if (size == "big")
-    {
-        return Results.Ok(new[] { Margherita(33), Cipolla(34), Funghi(34), Salami(36), FunghiEProsciutto(36) });
-    }
-
-    return Results.NotFound();
-});
-
-app.MapGet("/drink", (string type) =>
-{
-    if (type == "beer")
-    {
-        return Results.Ok(new[] { CreateDrink("Kozel Lezak", 10), CreateDrink("Żywiec Klasyk", 11) });
-    }
-
-    if (type == "mulled-wine")
-    {
-        return Results.Ok(new[]
-        {
-            CreateDrink("Grzaniec XXL 0.5L", 19), CreateDrink("Grzaniec Duży", 14),
-            CreateDrink("Grzaniec Mały", 10)
-        });
-    }
-
-    if (type == "other")
-    {
-        return Results.Ok(new[]
-        {
-            CreateDrink("Wino Czerwone", 15, new[] { "Kieliszek" }),
-            CreateDrink("Wino Białe", 15, new[] { "Kieliszek" }), CreateDrink("Kawa Czarna", 8),
-            CreateDrink("Herbata", 5, new[] { "Filiżanka" }), CreateDrink("Herbata", 9, new[] { "Dzbanek" })
-        });
-    }
-
-    return Results.NotFound();
-});
-
-app.MapGet("/snack", () =>
+app.MapGet("/menu", () =>
 {
     return Results.Ok(new[]
     {
-        //TODO: Extend filter to support this case
-        new ProductModel("CALZONE pieróg z nadzieniem", new []{"w każdym pierożku, ser, sos czosnkowy + 4 dowolne składniki do wyboru"}, 27),
-        new ProductModel("Frytki Cieńkie Małe", new []{"Porcja chrupiących frytek, ketchup"}, 8),
-        new ProductModel("Frytki Cieńkie Duże", new []{"Porcja chrupiących frytek, ketchup"}, 10),
-        new ProductModel("Frytki Belgijskie Małe", new []{"Porcja chrupiących frytek, ketchup"}, 8),
-        new ProductModel("Frytki Belgijskie Duże", new []{"Porcja chrupiących frytek, ketchup"}, 10)
-    });
-});
-
-app.MapGet("/sauce", () =>
-{
-    return Results.Ok(new[]
-    {
-        //TODO: Extend filter to support this case
-        new ProductModel("Czosnkowy", new []{"firmowy sos XXX idealnie komponujący się ze smakiem każdej naszej pizzy"}, 4),
-        new ProductModel("Czosnkowo-szczypiorkowy", new []{"firmowy sos XXX, ketchup"}, 4),
-        new ProductModel("Pomidorowy łagodny", new []{""}, 4)
+        CreateDrink("Kozel Lezak", 10),
+        CreateDrink("Żywiec Klasyk", 11),
+        CreateDrink("Grzaniec XXL 0.5L", 19),
+        CreateDrink("Grzaniec Duży", 14),
+        CreateDrink("Grzaniec Mały", 10),
+        new ProductModel("CALZONE pieróg z nadzieniem", new []{"w każdym pierożku, ser, sos czosnkowy + 4 dowolne składniki do wyboru"}, 27, ProductType.Snack),
+        new ProductModel("Frytki Cieńkie Małe", new []{"Porcja chrupiących frytek, ketchup"}, 8, ProductType.Snack),
+        new ProductModel("Frytki Cieńkie Duże", new []{"Porcja chrupiących frytek, ketchup"}, 10, ProductType.Snack),
+        new ProductModel("Frytki Belgijskie Małe", new []{"Porcja chrupiących frytek, ketchup"}, 8, ProductType.Snack),
+        new ProductModel("Frytki Belgijskie Duże", new []{"Porcja chrupiących frytek, ketchup"}, 10, ProductType.Snack),
+        Margherita(17, ProductType.PizzaSmall), Cipolla(18, ProductType.PizzaSmall), Funghi(18, ProductType.PizzaSmall), Salami(18, ProductType.PizzaSmall), FunghiEProsciutto(18, ProductType.PizzaSmall),
+        Margherita(22, ProductType.PizzaMedium), Cipolla(23, ProductType.PizzaMedium), Funghi(23, ProductType.PizzaMedium), Salami(26, ProductType.PizzaMedium), FunghiEProsciutto(26, ProductType.PizzaMedium),
+        Margherita(33, ProductType.PizzaBig), Cipolla(34, ProductType.PizzaBig), Funghi(34, ProductType.PizzaBig), Salami(36, ProductType.PizzaBig), FunghiEProsciutto(36, ProductType.PizzaBig),
+        new ProductModel("Czosnkowy", new []{"firmowy sos XXX idealnie komponujący się ze smakiem każdej naszej pizzy"}, 4, ProductType.Sauce),
+        new ProductModel("Czosnkowo-szczypiorkowy", new []{"firmowy sos XXX, ketchup"}, 4, ProductType.Sauce),
+        new ProductModel("Pomidorowy łagodny", new []{""}, 4, ProductType.Sauce)
     });
 });
 
@@ -97,21 +45,21 @@ app.MapGet("/delivery/{deliveryId}", (Guid deliveryId) =>
 
 app.Run();
 
-static ProductModel Margherita(double price) => new ProductModel("Margherita", new[] { "ser", "sos pomidorowy" }, price);
+static ProductModel Margherita(double price, ProductType productType) => new ProductModel("Margherita", new[] { "ser", "sos pomidorowy" }, price, productType);
 
-static ProductModel Cipolla(double price) =>
-    new ProductModel("Cipolla", new[] { "ser", "sos pomidorowy", "cebula" }, price);
+static ProductModel Cipolla(double price, ProductType productType) =>
+    new ProductModel("Cipolla", new[] { "ser", "sos pomidorowy", "cebula" }, price, productType);
 
-static ProductModel Funghi(double price) =>
-    new ProductModel("Funghi", new[] { "ser", "sos pomidorowy", "pieczarki" }, price);
+static ProductModel Funghi(double price, ProductType productType) =>
+    new ProductModel("Funghi", new[] { "ser", "sos pomidorowy", "pieczarki" }, price, productType);
 
-static ProductModel Salami(double price) => new ProductModel("Salami", new[] { "ser", "sos pomidorowy", "salami" }, price);
+static ProductModel Salami(double price, ProductType productType) => new ProductModel("Salami", new[] { "ser", "sos pomidorowy", "salami" }, price, productType);
 
-static ProductModel FunghiEProsciutto(double price) => new ProductModel("Funghi e Prosciutto",
-    new[] { "ser", "sos pomidorowy", "szynka", "pieczarki" }, price);
+static ProductModel FunghiEProsciutto(double price, ProductType productType) => new ProductModel("Funghi e Prosciutto",
+    new[] { "ser", "sos pomidorowy", "szynka", "pieczarki" }, price, productType);
 
 ProductModel CreateDrink(string name, double price, string[]? ingredients = null) =>
-    new ProductModel(name, ingredients ?? Array.Empty<string>(), price);
+    new ProductModel(name, ingredients ?? Array.Empty<string>(), price, ProductType.Drink);
 
 class ProductModel
 {
@@ -119,14 +67,26 @@ class ProductModel
     public string Name { get; set; }
     public string[] Ingredients { get; set; }
     public double Price { get; set; }
+    public ProductType ProductType { get; set; }
 
-    public ProductModel(string name, string[] ingredients, double price)
+    public ProductModel(string name, string[] ingredients, double price, ProductType productType)
     {
         Name = name;
         Ingredients = ingredients;
         Price = price;
         Id = Guid.NewGuid();
+        ProductType = productType;
     }
+}
+
+enum ProductType
+{
+    PizzaSmall,
+    PizzaMedium,
+    PizzaBig,
+    Drink,
+    Sauce,
+    Snack
 }
 
 class PaymentModel
