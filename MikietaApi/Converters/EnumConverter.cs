4324e9a286e08e9221f09104928277d9d445b109
ptrwilk <90389270+ptrwilk@users.noreply.github.com>
2024-05-13 21:58:59 +0200

    MIK-100 Get Zamowienia and Produkty from API

---
+++ MikietaApi/Converters/EnumConverter.cs
@@ -0,0 +1,10 @@
+﻿using MikietaApi.Data.Entities;
+using MikietaApi.Models;
+
+namespace MikietaApi.Converters;
+
+public static class EnumConverter
+{
+    public static ProductType Convert(ProductTypeEntity type) =>
+        (ProductType)Enum.Parse(typeof(ProductType), type.ToString());
+}
