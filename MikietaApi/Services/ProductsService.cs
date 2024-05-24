82465ca0aaad17cda7560ce3f2ad2cccbbe8e418
ptrwilk <90389270+ptrwilk@users.noreply.github.com>
2024-05-25 00:47:00 +0200

    MIK-122 Related to create products table

--- MikietaApi/Services/ProductsService.cs
+++ MikietaApi/Services/ProductsService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MikietaApi.Converters;
 using MikietaApi.Data;
 using MikietaApi.Data.Entities;
 using MikietaApi.Models;
@@ -8,6 +9,8 @@ namespace MikietaApi.Services;
 public interface IProductsService
 {
     ProductModel[] Get();
+    AdminProductModel2[] GetAdminProducts();
+    AdminProductModel2 UpdateAdminProduct(AdminProductModel2 model);
 }
 
 public class ProductsService : IProductsService
@@ -26,13 +29,41 @@ public class ProductsService : IProductsService
         return products.Select(Convert).ToArray();
     }
 
+    public AdminProductModel2[] GetAdminProducts()
+    {
+        var products = _context.Products.ToList();
+
+        return products.Select(entity => new AdminProductModel2
+        {
+            Id = entity.Id,
+            ProductType = EnumConverter.Convert(entity.ProductType),
+            Description = entity.Description,
+            Name = entity.Name,
+            Price = entity.Price
+        }).ToArray();
+    }
+
+    public AdminProductModel2 UpdateAdminProduct(AdminProductModel2 model)
+    {
+        var product = _context.Products.First(x => x.Id == model.Id);
+
+        product.Name = model.Name;
+        product.Description = string.IsNullOrWhiteSpace(model.Description) ? null : model.Description;
+        product.Price = model.Price;
+        product.ProductType = EnumConverter.Convert(model.ProductType);
+
+        _context.SaveChanges();
+        
+        return model;
+    }
+
     private ProductModel Convert(ProductEntity entity)
     {
         return new ProductModel
         {
             Id = entity.Id,
             Ingredients = entity.Description != null ? new []{ entity.Description} : entity.Ingredients.Select(x => x.Name).ToArray(),
-            ProductType = (ProductType)Enum.Parse(typeof(ProductType), entity.ProductType.ToString()),
+            ProductType = EnumConverter.Convert(entity.ProductType),
             Name = entity.Name,
             Price = entity.Price
         };
