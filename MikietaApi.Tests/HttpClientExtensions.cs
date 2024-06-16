78975a27ac2f9caf9592e20f8db5954598061437
ptrwilk <90389270+ptrwilk@users.noreply.github.com>
2024-06-16 22:11:29 +0200

    MIK-161 added validation check + unit tests for checking ProductQuantities ids

---
+++ MikietaApi.Tests/HttpClientExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace MikietaApi.Tests
+{
+    public static class HttpClientExtensions
+    {
+        public static async Task<TResult> As<TResult>(this Task<HttpResponseMessage> response)
+        {
+            var res = await response.ConfigureAwait(false);
+
+            if (res.StatusCode != HttpStatusCode.OK)
+            {
+                throw new Exception($"Received status code: {res.StatusCode}");
+            }
+
+            var stringContent = await res.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            return JsonSerializer.Deserialize<TResult>(stringContent, options)!;
+        }
+
+        public static StringContent ToStringContent(this object obj)
+        {
+            return new StringContent(JsonSerializer.Serialize(obj), Encoding.UTF8, "application/json");
+        }
+    }
+}
