faf1fa56fda830328baa4b2122aaefd33094cdff
ptrwilk <90389270+ptrwilk@users.noreply.github.com>
2024-08-10 17:43:36 +0200

    MIK-238 Added Admin Credentials to the configuration

--- MikietaApi/Services/LoginService.cs
+++ MikietaApi/Services/LoginService.cs
@@ -11,15 +11,17 @@ interface ILoginService
 public class LoginService : ILoginService
 {
     private readonly IJwtTokenFactory _jwtFactory;
+    private readonly ConfigurationOptions _options;
 
-    public LoginService(IJwtTokenFactory jwtFactory)
+    public LoginService(IJwtTokenFactory jwtFactory, ConfigurationOptions options)
     {
         _jwtFactory = jwtFactory;
+        _options = options;
     }
     
     public string Login(LoginModel model)
     {
-        if (model.Login == "admin" && model.Password == "admin")
+        if (model.Login == _options.AdminLogin && model.Password == _options.AdminPassword)
         {
             return _jwtFactory.Create(Guid.NewGuid().ToString());
         }
